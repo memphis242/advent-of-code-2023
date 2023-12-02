c5feebe71bbb1ba81e1f531951b08d190a7fae0d
C85HUP8 <AlmosalamiAbdullahI@JohnDeere.com>
2023-12-02 13:11:12 -0600

    contest: day 2 part 1 complete!

--- day2/python/aoc-day2.py
+++ day2/python/aoc-day2.py
@@ -84,7 +84,27 @@ with open(PUZZLE_INPUT, 'r') as puzzle_input:
 # print( get_game_line_id( puzzle_input_lines[85] ) )
 # print( color_maxes_in_line( puzzle_input_lines[87], '1' ) )
 
-# game_color_maxes = []
-# for line in puzzle_input_lines:
-#    # find the max of each color in each line
-#    game_color_maxes.append( color_maxes_in_line(line) )
+# get game id's and color maxes for each game
+game_ids = []  # will be list of numbers representing game id's
+game_color_maxes = []   # will be a list of dictionaries of the max of each color for each game
+for line in puzzle_input_lines:
+   # get game id
+   game_id = get_game_line_id(line)
+   game_ids.append(game_id)
+   # find the max of each color in each line
+   game_color_maxes.append( color_maxes_in_line( line, str(game_id) ) )
+
+possible_games = []  # will be list of numbers representing possible game id's
+for game_id, game_max_dict in zip(game_ids, game_color_maxes):
+   if game_max_dict['red']    <= PROBLEM_STATEMENT_MAXES['red']   and \
+      game_max_dict['blue']   <= PROBLEM_STATEMENT_MAXES['blue']  and \
+      game_max_dict['green']  <= PROBLEM_STATEMENT_MAXES['green']:
+
+      possible_games.append(game_id)
+
+# sum over games!
+total = 0
+for game_id in possible_games:
+   total = total + game_id
+
+print(total)
